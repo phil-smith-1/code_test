b8df09e6624eda38054b62f09af2f4ccc94303cc
Phil smith <pcsmith86@gmail.com>
2018-06-28 11:49:03 +0100

    Updated validation and added additional specs

--- spec/controller/callback_controller_spec.rb
+++ spec/controller/callback_controller_spec.rb
@@ -11,14 +11,10 @@ RSpec.describe CallbackController, type: :controller do
       }
     end
 
-    let(:invalid_params) do
-      {
-        name: 'Dave Newson',
-        business_name: 'Dave Newson LTD',
-        telephone_number: 'not_a_number',
-        email: 'dave.newson@deavenewson.co.uk'
-      }
-    end
+    let(:invalid_name) { { name: (0..100).map { 'A' }.join } }
+    let(:invalid_business_name) { { business_name: (0..100).map { 'A' }.join } }
+    let(:invalid_telephone_number) { { telephone_number: 'not_a_number' } }
+    let(:invalid_email) { { email: 'not_a_valid_email_address' } }
 
     context 'When the data is valid' do
       before do
@@ -35,18 +31,36 @@ RSpec.describe CallbackController, type: :controller do
       end
     end
 
-    context 'When the data is invalid' do
+    context 'When the name is invalid' do
       before do
-        post :submit, params: invalid_params
+        post :submit, params: valid_params.merge(invalid_name)
       end
 
-      it 'renders the form' do
-        expect(controller).to render_template(:new)
+      include_examples('error_examples', 'One or more field is invalid. Please check the form')
+    end
+
+    context 'When the business name is invalid' do
+      before do
+        post :submit, params: valid_params.merge(invalid_business_name)
       end
 
-      it 'sets the flash to the invalid data message' do
-        expect(controller).to set_flash.now.to('One or more field is invalid. Please check the form')
+      include_examples('error_examples', 'One or more field is invalid. Please check the form')
+    end
+
+    context 'When the telephone number is invalid' do
+      before do
+        post :submit, params: valid_params.merge(invalid_telephone_number)
       end
+
+      include_examples('error_examples', 'One or more field is invalid. Please check the form')
+    end
+
+    context 'When the email is invalid' do
+      before do
+        post :submit, params: valid_params.merge(invalid_email)
+      end
+
+      include_examples('error_examples', 'One or more field is invalid. Please check the form')
     end
 
     context 'When there is a problem with the API' do
@@ -55,13 +69,7 @@ RSpec.describe CallbackController, type: :controller do
         post :submit, params: valid_params
       end
 
-      it 'renders the form' do
-        expect(controller).to render_template(:new)
-      end
-
-      it 'sets the flash to the error message' do
-        expect(controller).to set_flash.now.to('We\'re having some technical difficulties at the moment. Please try again in a few minutes')
-      end
+      include_examples('error_examples', 'We\'re having some technical difficulties at the moment. Please try again in a few minutes')
     end
   end
 end
---
+++ spec/support/shared_examples/callback_controller_examples.rb
@@ -0,0 +1,9 @@
+RSpec.shared_examples "error_examples" do |message|
+  it 'renders the form' do
+    expect(controller).to render_template(:new)
+  end
+
+  it 'sets the flash to the error message' do
+    expect(controller).to set_flash.now.to(message)
+  end
+end
