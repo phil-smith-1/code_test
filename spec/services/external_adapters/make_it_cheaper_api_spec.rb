2ca3e3a1fef41e82b20f2226b5137f3263906b9e
Phil smith <pcsmith86@gmail.com>
2018-06-28 11:47:53 +0100

    Updated VCR config and external api spec

--- spec/services/external_adapters/make_it_cheaper_api_spec.rb
+++ spec/services/external_adapters/make_it_cheaper_api_spec.rb
@@ -1,23 +1,42 @@
 require "rails_helper"
 
 RSpec.describe ExternalAdapters::MakeItCheaperApi, :vcr do
+  let(:name) { "Dave Newson" }
+  let(:business_name) { "Dave Newson LTD" }
+  let(:phone) { "07654 321 123" }
+  let(:email) { "dave.newson@deavenewson.co.uk" }
+  let(:params) do
+    {
+      name: name,
+      business_name: business_name,
+      telephone_number: phone,
+      email: email
+    }
+  end
+
   describe '.post' do
     it "returns success if the customer data is correct" do
-      response = described_class.post("api/v1/create", params)
-      expect(response.message).to eq("Enqueue success")
-      expect(response.errors).to eq([])
+      VCR.use_cassette("success") do
+        response = described_class.post("api/v1/create", params)
+        expect(response.parsed_response["message"]).to eq("Enqueue success")
+        expect(response.parsed_response["errors"]).to eq([])
+      end
     end
 
     it "returns an error if the request data is invalid" do
-      expect {
-        described_class.post("api/v1/create")
-      }.to raise_error(ExternalAdapters::MakeItCheaperApi::ValidationError)
+      VCR.use_cassette("validation_error") do
+        expect {
+          described_class.post("api/v1/create")
+        }.to raise_error(ExternalAdapters::MakeItCheaperApi::ValidationError)
+      end
     end
 
     it "returns an error if the request url doesn't exist" do
-      expect {
-        described_class.post("wrong-path", params)
-      }.to raise_error(ExternalAdapters::MakeItCheaperApi::FetchError)
+      VCR.use_cassette("bad_request") do
+        expect {
+          described_class.post("wrong-path", params)
+        }.to raise_error(ExternalAdapters::MakeItCheaperApi::FetchError)
+      end
     end
   end
 end
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,5 +2,6 @@
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/cassettes'
+  config.default_cassette_options = { record: :new_episodes }
   config.hook_into :webmock
 end
